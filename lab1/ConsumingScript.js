6f978231c7f41f64fadf88eb93fb676c6804e27d
Abdelrahman Ahmed <abdelrahmanrashed136@gmail.com>
2025-04-30 20:54:17 +0300

    lab2 Js solution

---
+++ lab1/ConsumingScript.js
@@ -0,0 +1,21 @@
+var values = printVariables(3, 5, 6, 7, 8, 9, 10, 11);
+console.log(values)
+// console.log(localVar);
+// console.log(testingVar);
+
+
+
+
+
+
+
+
+
+
+
+// console.log(values[0])
+// console.log(values[1])
+// console.log(values[2])
+// console.log(value1)
+// console.log(value2)
+// console.log(value3)
