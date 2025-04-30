13f3b5b4fc882053ef59c0b2cc8755b89240f0ba
Abdelrahman136 <96352166+Abdelrahman136@users.noreply.github.com>
2025-04-30 23:19:55 +0300

    Solved Lab 3 JS ITi

---
+++ lab3/consumingScript.js
@@ -0,0 +1,9 @@
+console.log(PascalCase('the quick brouwn fox'));
+
+console.log(getLongestWord('Web Development Tutorial'));
+
+console.log(alphabeticalOrder('javascript'));
+
+console.log(getCommenElement([1, 2, 3], [2, 3, 4]));
+
+console.log(removeDuplicates([1, 2, 3, 2, 5, 2, 1, 0]))
